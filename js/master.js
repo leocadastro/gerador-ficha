5a1c78e2e49db1ae3e2fb76da19632231f23a2e7
Leonardo Cadastro <leonardocadastro69@gmail.com>
2017-05-21 18:17:35 -0300

    corrigindo input

--- js/master.js
+++ js/master.js
@@ -1,37 +1,33 @@
-function jsfunction(){
-    $("#object5").text($("#valor-selecionado").val())
+function jsfunction() {
+  $("#object5").text($("#valor-selecionado").val())
 }
 
-function jsfunction2(){
-    $("#object5").text($("#valor-texto").val())
+function jsfunction2() {
+  $("#object5").text($("#valor-texto").val())
 }
-
-$( document ).ready(function() {
-    $('input[id=valor-texto]').mask('##,##');
-});
 
 function jsPrint(){
   window.print();
 }
 
+//TODO
 function jsCalc(valor) {
   let valores = [20, 10, 5, 2];
   let fichas = [];
+  let aux = 0;
 
   for (var i = 0; i < valores.length; i++) {
-    while (valor % valores[i] == 0) {
+    while (valor % valores[i] == 0 && valor != 0 && aux < 3) {
       fichas.push(valores[i]);
-      valor = valor - valores[i];
-      console.log(valores[i]);
+      valor = valor / valores[i];
+      aux++;
     }
+    aux=0;
   }
-
-  // if (valor % valorFicha == 0) {
-  //   while (valor % valorFicha == 0) {
-  //     fichas.push(valorFicha);
-  //     valor = valor/valorFicha;
-  //   }
-  // }
 
   return fichas;
 }
+
+$(document).ready(function() {
+  //  $('input[id=valor-texto]').mask('#,##');
+});
